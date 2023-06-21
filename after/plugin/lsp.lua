ba0e157b85531838b2a8570b2c98724f4885fb62
Kilian Vounckx <kilianvounckx@hotmail.be>
2023-06-21 12:30:13 +0200

    Remove gl and gh keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,8 +32,6 @@ lsp.on_attach(function(client, bufnr)
 	-- only set keymaps in current buffer
 	local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gl", "g_", opts)
-
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
