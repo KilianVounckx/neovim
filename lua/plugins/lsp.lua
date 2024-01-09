2d5e117ce1846a2e16f23543bbc89b974529417c
KilianVounckx <kilianvounckx@hotmail.be>
2024-01-09 20:00:05 +0100

    :technologist: Change autocomplete keybinds to be more what I am used to

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,10 +35,9 @@ return {
             cmp.setup({
                 formatting = lsp_zero.cmp_format(),
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ["<S-tab>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<tab>"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<cr>"] = cmp.mapping.confirm({ select = true }),
                 })
             })
         end
