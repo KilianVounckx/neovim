c2e00e31ff31336c7aeb41ba91b3e2e5abf089e1
KilianVounckx <kilianvounckx@hotmail.be>
2024-01-12 16:38:58 +0100

    :technologist: Change terminal size and add shortcut for resizing

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,13 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
         require("toggleterm").setup {
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return vim.o.lines * 0.3
+                else
+                    return vim.o.columns * 0.4
+                end
+            end,
             open_mapping = "<leader>tt",
             hide_numbers = true,
             shade_filetypes = {},
@@ -19,6 +26,8 @@ return {
             local opts = { buffer = 0 }
             vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
             vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+            vim.keymap.set("n", "+", "<C-w>+", opts)
+            vim.keymap.set("n", "-", "<C-w>-", opts)
         end
 
         vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
