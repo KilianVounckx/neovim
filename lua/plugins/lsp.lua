0947e5084acc54bc9e78cd1f510913b294f6944b
KilianVounckx <kilianvounckx@hotmail.be>
2024-11-07 09:01:25 +0100

    :sparkles: Add rust_analyzer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,63 +30,73 @@ local lua_ls = {
     },
 }
 
+local rust_analyzer = {
+    settings = {
+        ["rust-analyzer"] = {
+            check = {
+                command = "clippy",
+            },
+        },
+    },
+}
+
 local tinymist = {}
 local typst_lsp = {}
 
 local function on_attach(event)
-        local function opts_with(opts)
-            local result = { buffer = event.buf }
-            for key, value in pairs(opts) do
-                result[key] = value
-            end
-            return result
+    local function opts_with(opts)
+        local result = { buffer = event.buf }
+        for key, value in pairs(opts) do
+            result[key] = value
         end
+        return result
+    end
 
-        vim.keymap.set(
-            "n", "<leader>k",
-            "<cmd>lua vim.lsp.buf.hover()<cr>",
-            opts_with({ desc = "Hover" })
-        )
-        vim.keymap.set(
-            "n", "gd",
-            "<cmd>lua vim.lsp.buf.definition()<cr>",
-            opts_with({ desc = "Goto definition" })
-        )
-        vim.keymap.set(
-            "n", "gD",
-            "<cmd>lua vim.lsp.buf.declaration()<cr>",
-            opts_with({ desc = "Goto declaration" })
-        )
-        vim.keymap.set(
-            "n", "go",
-            "<cmd>lua vim.lsp.buf.type_definition()<cr>",
-            opts_with({ desc = "Goto type definition" })
-        )
-        vim.keymap.set(
-            "n", "gr",
-            "<cmd>lua vim.lsp.buf.references()<cr>",
-            opts_with({ desc = "Goto references" })
-        )
-        vim.keymap.set(
-            "n", "gs",
-            "<cmd>lua vim.lsp.buf.signature_help()<cr>",
-            opts_with({ desc = "Show signature help" })
-        )
-        vim.keymap.set(
-            "n", "<leader>r",
-            "<cmd>lua vim.lsp.buf.rename()<cr>",
-            opts_with({ desc = "Rename" })
-        )
-        vim.keymap.set(
-            { "n", "x" }, "<F3>",
-            "<cmd>lua vim.lsp.buf.format({ async = true })<cr>",
-            opts_with({ desc = "Format" })
-        )
-        vim.keymap.set(
-            { "n", "x" }, "<leader>a",
-            "<cmd>lua vim.lsp.buf.code_action()<cr>",
-            opts_with({ desc = "Show actions" })
-        )
+    vim.keymap.set(
+        "n", "<leader>k",
+        "<cmd>lua vim.lsp.buf.hover()<cr>",
+        opts_with({ desc = "Hover" })
+    )
+    vim.keymap.set(
+        "n", "gd",
+        "<cmd>lua vim.lsp.buf.definition()<cr>",
+        opts_with({ desc = "Goto definition" })
+    )
+    vim.keymap.set(
+        "n", "gD",
+        "<cmd>lua vim.lsp.buf.declaration()<cr>",
+        opts_with({ desc = "Goto declaration" })
+    )
+    vim.keymap.set(
+        "n", "go",
+        "<cmd>lua vim.lsp.buf.type_definition()<cr>",
+        opts_with({ desc = "Goto type definition" })
+    )
+    vim.keymap.set(
+        "n", "gr",
+        "<cmd>lua vim.lsp.buf.references()<cr>",
+        opts_with({ desc = "Goto references" })
+    )
+    vim.keymap.set(
+        "n", "gs",
+        "<cmd>lua vim.lsp.buf.signature_help()<cr>",
+        opts_with({ desc = "Show signature help" })
+    )
+    vim.keymap.set(
+        "n", "<leader>r",
+        "<cmd>lua vim.lsp.buf.rename()<cr>",
+        opts_with({ desc = "Rename" })
+    )
+    vim.keymap.set(
+        { "n", "x" }, "<F3>",
+        "<cmd>lua vim.lsp.buf.format({ async = true })<cr>",
+        opts_with({ desc = "Format" })
+    )
+    vim.keymap.set(
+        { "n", "x" }, "<leader>a",
+        "<cmd>lua vim.lsp.buf.code_action()<cr>",
+        opts_with({ desc = "Show actions" })
+    )
 end
 
 return {
@@ -135,8 +145,18 @@ return {
 
             setup("hls", hls)
             setup("lua_ls", lua_ls)
+            setup("rust_analyzer", rust_analyzer)
             setup("tinymist", tinymist)
             setup("typst_lsp", typst_lsp)
+
+            vim.lsp.inlay_hint.enable()
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                desc = "LSP Format",
+                callback = function(event)
+                    vim.lsp.buf.format({})
+                end,
+            })
 
             vim.api.nvim_create_autocmd("LspAttach", {
                 desc = "LSP Actions",
